26169843fca3607f99ccd0524d7db38194e3958f
a-westphal <west8659@stthomas.edu>
2019-12-14 23:30:17 -0600

    lat lon

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -200,13 +200,13 @@ function Init(crime_api_url) {
 				var search = geocode + block +"+St+Paul+MN&addressdetails=1&limit=1";
 				console.log(search);
 				$.getJSON(geocode, function(data){
-					console.log(data);
-					//var block_lat = data[0]['lat'];
-					//var block_lon = data[0]['lon'];
-					//console.log("lat: " + block_lat);
-					//console.log("lon: " + block_lon);
-					//var list = "<dl><dt>Neighborhood</dt>" + "<dd>" + incident['neighborhood_name'] + "</dd>" + "<dt>Incident:</dt>" + "<dd>" + incident['incident'] + "</dd>" + "<dt> Date: </dt>" +"<dd>" +incident['date'] +"</dd>"
-					//L.marker([block_lat,block_lon]).bindPopup(list).addTo(markersLayer);
+					//console.log(data);
+					var block_lat = data[0]['lat'];
+					var block_lon = data[0]['lon'];
+					console.log("lat: " + block_lat);
+					console.log("lon: " + block_lon);
+					var list = "<dl><dt>Neighborhood</dt>" + "<dd>" + incident['neighborhood_name'] + "</dd>" + "<dt>Incident:</dt>" + "<dd>" + incident['incident'] + "</dd>" + "<dt> Date: </dt>" +"<dd>" +incident['date'] +"</dd>"
+					L.marker([block_lat,block_lon]).bindPopup(list).addTo(markersLayer);
 				})
 
 			}			
